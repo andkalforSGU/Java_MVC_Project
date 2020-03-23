53e1b8e83fc4e7d011e5c61b34ee8b7dbaf8c3f2
Andrey <andkalforSGU@yandex.ru>
2020-03-23 22:55:53 +0400

    V--0.2(Full)

--- src/main/java/com/example/servingwebcontent/MainController.java
+++ src/main/java/com/example/servingwebcontent/MainController.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
 
     @Autowired
-    private MessageRepos massagesRepos;
+    private MessageRepos messagesRepos;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
@@ -24,19 +25,37 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        Iterable<Massage> messages = massagesRepos.findAll();
+        Iterable<Massage> messages = messagesRepos.findAll();
 
         model.put("messages", messages);
         return "main";
     }
 
+
+
     @PostMapping
     public String add(Map<String, Object> model, @RequestParam String text, @RequestParam String tag){
         Massage message = new Massage(text, tag);
-        massagesRepos.save(message);
-        Iterable<Massage> massages = massagesRepos.findAll();
+        messagesRepos.save(message);
+        Iterable<Massage> massages = messagesRepos.findAll();
 
         model.put("messages", massages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(Map<String, Object> model, @RequestParam String filter) {
+
+        List<Massage> massages = messagesRepos.findByTag(filter);
+
+        model.put("messages", massages);
+        return "main";
+    }
+
+    @PostMapping("home")
+    public String home(Map<String, Object> model){
+        Iterable<Massage> messages = messagesRepos.findAll();
+        model.put("messages", messages);
         return "main";
     }
 }
--- src/main/java/com/example/servingwebcontent/repos/MessageRepos.java
+++ src/main/java/com/example/servingwebcontent/repos/MessageRepos.java
@@ -3,6 +3,10 @@ package com.example.servingwebcontent.repos;
 import com.example.servingwebcontent.entities.Massage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepos extends CrudRepository<Massage, Integer> {
+
+    List<Massage>findByTag(String tag);
 
 }
