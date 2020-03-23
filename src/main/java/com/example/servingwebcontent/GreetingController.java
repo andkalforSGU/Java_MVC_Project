1b727062f78f8ba662ea51a2843bca4fe6462020
Andrey <andkalforSGU@yandex.ru>
2020-03-23 14:49:20 +0400

    first commit

---
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -0,0 +1,44 @@
+package com.example.servingwebcontent;
+
+import com.example.servingwebcontent.entities.Massage;
+import com.example.servingwebcontent.repos.MassageRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @Autowired
+    private MassageRepos massagesRepos;
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        Iterable<Massage> massages = massagesRepos.findAll();
+
+        model.put("massages", massages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(Map<String, Object> model, @RequestParam String text, @RequestParam String tag){
+        Massage massage = new Massage(text, tag);
+        massagesRepos.save(massage);
+
+        Iterable<Massage> massages = massagesRepos.findAll();
+
+        model.put("massages", massages);
+        return "main";
+    }
+
+}
---
+++ src/main/java/com/example/servingwebcontent/repos/MassageRepos.java
@@ -0,0 +1,8 @@
+package com.example.servingwebcontent.repos;
+
+import com.example.servingwebcontent.entities.Massage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MassageRepos extends CrudRepository<Massage, Integer> {
+
+}
