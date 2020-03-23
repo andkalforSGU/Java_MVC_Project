786650ab43748752041bde27799c51a2272273c3
Andrey <andkalforSGU@yandex.ru>
2020-03-23 20:30:43 +0400

    Ver--0.1

--- src/main/java/com/example/servingwebcontent/repos/MessageRepos.java
+++ src/main/java/com/example/servingwebcontent/repos/MessageRepos.java
@@ -3,6 +3,6 @@ package com.example.servingwebcontent.repos;
 import com.example.servingwebcontent.entities.Massage;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MassageRepos extends CrudRepository<Massage, Integer> {
+public interface MessageRepos extends CrudRepository<Massage, Integer> {
 
 }
